eed4ce928d85ed853e56a8d47914cd515795f573
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-19 22:12:15 -0400

    use keys to keep track of elements

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const players = [
   {
     name: "Guil",
-    score: 50
+    score: 50,
+    id: 1
   },
   {
     name: "Treasure",
-    score: 85
+    score: 85,
+    id: 2
   },
   {
     name: "Ashley",
-    score: 95
+    score: 95,
+    id: 3
   },
   {
     name: "James",
-    score: 80
+    score: 80,
+    id: 4
   }
 ];
 
@@ -56,10 +60,11 @@ const App = props => {
         totalPlayers={ props.initialPlayers.length }
       />
       {/*Players list*/}
-      {props.initialPlayers.map( player =>
+      {props.initialPlayers.map(player =>
         <Player
           name={ player.name }
           score={ player.score }
+          key={ player.id.toString() }
         />
       )}
     </div>
